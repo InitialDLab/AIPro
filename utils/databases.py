a92e48e2b942b8571c9639b4323c44efa1db1cb2
Richie Frost <richard.scott.frost@gmail.com>
2017-12-11 10:02:52 -0700

    Finished MongoDB connector

--- utils/databases.py
+++ utils/databases.py
@@ -1,35 +1,57 @@
 from pymongo.mongo_client import MongoClient
 
 class MongoConnection:
-	def __init__(self, conn_url):
-		self.conn = MongoClient(conn_url)
+	def __init__(self, db_name, host='localhost', port=27017):
+		self.client = MongoClient(host, port)
+		self.db = client[db_name]
 
 	def insert_one(self, collection, document):
-		pass
+		current_collection = self.db[collection]
+		result = current_collection.insert_one(document)
+		return result.inserted_id
 
 	def insert_many(self, collection, documents):
-		pass
+		current_collection = self.db[collection]
+		result = current_collection.insert_many(documents)
+		return result.inserted_ids
 
-	def update_one(self, collection, filter, update, upsert=False):
-		pass
+	def update_one(self, collection, query_filter, update, upsert=False):
+		current_collection = self.db[collection]
+		result = current_collection.update_one(query_filter, update, upsert=upsert)
+		return result.modified_count
 
-	def update_many(self, collection, filter, update, upsert=False):
-		pass
+	def update_many(self, collection, query_filter, update, upsert=False):
+		current_collection = self.db[collection]
+		result = current_collection.update_many(query_filter, update, upsert=upsert)
+		return result.modified_count
 
-	def delete_one(self, collection, filter):
-		pass
+	def delete_one(self, collection, query_filter):
+		current_collection = self.db[collection]
+		result = current_collection.delete(query_filter)
+		return result.deleted_count
 
-	def delete_many(self, collection, filter):
-		pass
+	def delete_many(self, collection, query_filter):
+		current_collection = self.db[collection]
+		result = current_collection.delete(query_filter)
+		return result.deleted_count		
 
-	def find(self, collection, filter=None, projection=None, sort=None, skip=0, limit=0):
-		pass
+	def find(self, collection, query_filter=None, projection=None, sort=None, skip=0, limit=0):
+		current_collection = self.db[collection]
+		cursor = current_collection.find(filter=query_filter, projection=projection, sort=sort, skip=skip, limit=limit)
+		for doc in cursor:
+			yield return doc
 
-	def find_one(self, collection, filter=None):
-		pass
+	def find_one(self, collection, query_filter=None, projection=None):
+		current_collection = self.db[collection]
+		result = current_collection.find_one(filter=query_filter, projection=projection)
+		return result
 
-	def count(self, collection, filter=None):
-		pass
+	def count(self, collection, query_filter=None, limit=0, skip=0):
+		current_collection = self.db[collection]
+		result = current_collection.count(filter=query_filter, limit=limit, skip=skip)
+		return result
 
-	def distinct(self, collection, key, filter=None):
-		pass
+	def distinct(self, collection, key, query_filter=None):
+		current_collection = self.db[collection]
+		result = current_collection.distinct(key, filter=query_filter)
+		return result
